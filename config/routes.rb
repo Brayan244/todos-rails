eb2db433a13fea304f13030c90b1956c4314136b
Brayan Zavala <zavalabrayan244@gmail.com>
2020-05-05 22:54:18 -0500

    adds todos model and some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
-  root "application#hello"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "pages#home"
+  get '/about', to: 'pages#about'
+  get '/help',  to: 'pages#help'
 end
